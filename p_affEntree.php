72dd7dc6c6a603580a5640a7f5c3f9671f95484b
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-03-06 17:25:21 +0100

    Bouton de retour au début

---
+++ p_affEntree.php
@@ -0,0 +1,22 @@
+<?php
+$nomTable = $_GET['nT'];
+$nomEnter = $_GET['id'];
+
+echo "<!DOCTYPE html>
+<html lang=\"fr\">
+    <head>
+        <meta charset=\"UTF-8\">
+        <title>AppTech</title>
+        <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">
+    </head>
+
+    <body>
+        <h1 id=\"mainTitle\">Modifier les informations</h1>
+    <br />
+
+//        <form action=\"action_afficherTable.php\" method=\"post\">
+//            <p><input class=\"basePolice round-broder\" style=\"width:800px\" type=\"text\" name=\"nom\" /></p>
+//            <p><input class=\"round-broder basePolice\" style=\"width:550px\" type=\"submit\" value=\"Voir le contenue de la table\"></p>
+//        </form>
+    </body>
+</html>";
