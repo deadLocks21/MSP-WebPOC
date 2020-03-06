b80bcbccedde7fb60bd3c2c0e149f260319b2e9d
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-03-06 15:48:55 +0100

    Affichage du contenu de la table choisie.

--- p_affTable.php
+++ p_affTable.php
@@ -1,30 +1,96 @@
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <?php
+<?php
 $nomTable = $_GET['nT'];
-$servername = "localhost";
-$username = "timothe_MSP";
-$password = "mdpDB=MSP21";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=timothe_MSP", $username, $password);
-
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-}
-catch(PDOException $e)
-{
-    echo "Connection failed: " . $e->getMessage();
-}?>
-<meta charset="UTF-8">
-<title>Contenue <?php echo $nomTable?></title>
-<link rel="stylesheet" type="text/css" href="style.css">
+function invokeStructureDeb($nT){
+    echo "<!DOCTYPE html>
+<html lang=\"fr\">
+    <head>
+        <meta charset=\"UTF-8\">
+        <title>Contenue $nT</title>
+        <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">
     </head>
 
     <body>
-        <h1 id="mainTitle">Contenue de la table <?php echo $nomTable?></h1>
-        <br />
+        <h1 id=\"mainTitle\">Contenue de la table $nT</h1>
+        <br />";
+}
 
-    </body>
-</html>
+function invokeStructureFin(){
+    echo "        </div>\n    </body>\n</html>";
+}
+
+function invokeDebList(){
+    echo "            <a>
+                <ul class=\"center\">";
+}
+
+function invokeFinList(){
+    echo "                </ul>
+            </a>
+";
+}
+
+function invokeLi($tC, $el){
+    echo "                    <li class=\"basePolice\" style=\"width:$tC%;\">$el</li>\n";
+}
+
+function dbCon() {
+    $servername = "localhost";
+    $username = "timothe_MSP";
+    $password = "mdpDB=MSP21";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=timothe_MSP", $username, $password);
+        return $conn;
+    }
+    catch(PDOException $e)
+    {
+        echo "Connection failed: " . $e->getMessage();
+    }
+}
+
+function detTailleCol($conn, $nomTable){
+    $results = $conn->query("SELECT count(*) FROM information_schema.COLUMNS WHERE table_schema = 'timothe_MSP' AND table_name='$nomTable'");
+
+    $tables = array();
+    while($row = $results->fetch()){
+        $tables[] = $row[0];
+    }
+
+    $nbCol = $tables[0];
+
+    return 90/$nbCol;
+}
+
+function recupData($conn, $nomTable) {
+    $results = $conn->query("SELECT * FROM $nomTable");
+
+    $n = $results->fetchAll();
+
+    $res = array();
+    foreach ($n as $val){
+        $res[] = $val;
+    }
+
+    return $res;
+}
+
+
+$conn = dbCon();
+$tailleCol = detTailleCol($conn, $nomTable);
+
+
+invokeStructureDeb($nomTable);
+echo "\n        <div class=\"results round-broder\">\n";
+
+$ctnCol = recupData($conn, $nomTable);
+
+foreach ($ctnCol as $ctn) {
+    invokeDebList();
+    for($i=0; $i<count($ctn); $i++){
+        invokeLi($tailleCol, $ctn[$i]);
+    }
+    invokeFinList();
+}
+
+invokeStructureFin();
