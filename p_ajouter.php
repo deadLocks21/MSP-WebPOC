62f4c0fb802e29904382d50fe377d6297643589c
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-03-09 11:19:46 +0100

    Menu Ajouter opérationnel.

--- p_ajouter.php
+++ p_ajouter.php
@@ -1,6 +1,63 @@
 <?php
 $tN = $_GET['nT'];
 
+
+
+
+
+function dbCon() {
+    $servername = "localhost";
+    $username = "timothe_MSP";
+    $password = "mdpDB=MSP21";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=timothe_MSP", $username, $password);
+        return $conn;
+    }
+    catch(PDOException $e)
+    {
+        echo "Connection failed: " . $e->getMessage();
+    }
+}
+
+function recupNomCol($conn, $nomTable) {
+    $results = $conn->query("SHOW COLUMNS FROM $nomTable");
+
+    $n = $results->fetchAll();
+
+    $res = array();
+    foreach ($n as $val){
+        $res[] = $val[0];
+    }
+
+    return $res;
+}
+
+function printForm(){
+    $c = dbCon();
+    $tN = $_GET['nT'];
+    $nC = recupNomCol($c, $tN);
+    $res = "";
+
+    foreach ($nC as $item){
+        $res = $res . "
+                <p class=\"basePolice round-broder\">$item :&nbsp<input class=\"round-broder\" style=\"width:800px;font-size: 1em\" type=\"text\" name=\"$item\" /></p>";
+    }
+
+    $res .= "
+                <p><input class=\"round-broder basePolice\" style=\"width:550px\" type=\"submit\" value=\"Ajouter les données\"></p>";
+
+    return $res;
+}
+
+
+
+
+
+
+
+$form = printForm();
+
 echo "<!DOCTYPE html>
 <html lang=\"fr\">
     <head>
@@ -16,6 +73,8 @@ echo "<!DOCTYPE html>
             
             <br />
             <form action=\"action_ajouter.php\" method=\"post\">
+            
+                $form
             
             </form>
         </div>
