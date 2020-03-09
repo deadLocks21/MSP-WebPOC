90853aefb469b924c0feaaf613a865a9e0c20442
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-03-09 10:52:14 +0100

    Apparence de base de la page d'ajout

--- p_ajouter.php
+++ p_ajouter.php
@@ -1,2 +1,23 @@
 <?php
-echo 'Ajouter';
+$tN = $_GET['nT'];
+
+echo "<!DOCTYPE html>
+<html lang=\"fr\">
+    <head>
+        <meta charset=\"UTF-8\">
+        <title>Contenue $tN</title>
+        <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">
+    </head>
+
+    <body>
+        <a href='p_affTable.php?nT=$tN' class=\"return\">Retour</a>
+        <div class=\"center\">
+            <h1 id=\"mainTitle\">Ajouter un élement à la table $tN</h1>
+            
+            <br />
+            <form action=\"action_ajouter.php\" method=\"post\">
+            
+            </form>
+        </div>
+    </body>
+</html>";
