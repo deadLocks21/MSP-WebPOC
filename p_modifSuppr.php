9a134d981fc64aa3326b5ea453e23b36cd14cd81
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-03-09 11:41:33 +0100

    Design du menu pour modifier et supprimer une donnée

--- p_modifSuppr.php
+++ p_modifSuppr.php
@@ -1,5 +1,96 @@
 <?php
-$nomTable = $_GET['tN'];
-$nomEnter = $_GET['id'];
+$tN = $_GET['tN'];
+$id = $_GET['id'];
 
-echo "Modifier ou supprimer";
+function dbCon() {
+    $servername = "localhost";
+    $username = "timothe_MSP";
+    $password = "mdpDB=MSP21";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=timothe_MSP", $username, $password);
+        return $conn;
+    }
+    catch(PDOException $e)
+    {
+        echo "Connection failed: " . $e->getMessage();
+    }
+}
+
+function recupNomCol($conn, $nomTable) {
+    $results = $conn->query("SHOW COLUMNS FROM $nomTable");
+
+    $n = $results->fetchAll();
+
+    $res = array();
+    foreach ($n as $val){
+        $res[] = $val[0];
+    }
+
+    return $res;
+}
+
+function recupData($conn, $nomTable) {
+    $results = $conn->query("SELECT * FROM $nomTable");
+
+    $n = $results->fetchAll();
+
+    $res = array();
+    foreach ($n as $val){
+        $res[] = $val;
+    }
+
+    return $res;
+}
+
+function printForm(){
+    $c = dbCon();
+    $tN = $_GET['tN'];
+    $id = $_GET['id'];
+    $nC = recupNomCol($c, $tN);
+    $res = "";
+    $d = recupData($c, $tN)[$id];
+
+    for($i = 0 ; $i < count($nC); $i++){
+        $res = $res . "
+                <p class=\"basePolice round-broder\">$nC[$i] :&nbsp<input class=\"round-broder\" style=\"width:800px;font-size: 1em\" type=\"text\" name=\"$nC[$i]\" value=\"$d[$i]\" /></p>";
+    }
+
+    $res .= "
+                <p><input class=\"round-broder basePolice\" style=\"width:550px\" type=\"submit\" value=\"Modifier les données\"></p>";
+
+    return $res;
+}
+
+
+
+
+
+
+
+$form = printForm();
+
+echo "<!DOCTYPE html>
+<html lang=\"fr\">
+    <head>
+        <meta charset=\"UTF-8\">
+        <title>Contenue $tN</title>
+        <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">
+    </head>
+
+    <body>
+        <a href='p_affTable.php?nT=$tN' class=\"return\">Retour</a>
+        <div class=\"center\">
+            <h1 id=\"mainTitle\">Modifier un élement de la table $tN</h1>
+
+            <br />
+            <form action=\"action_ajouter.php\" method=\"post\">
+            
+                $form
+
+            </form>
+            
+            <a class=\"basePolice button\"href='action_supprimer.php?tN=$tN&id=$id' style=\"background-color: #CCCCCC;\">Supprimer l'élement</a>
+        </div>
+    </body>
+</html>";
